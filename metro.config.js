73f8852e0dba997a5bfda6b40129662560005adf
Brayden <braydenrhancock@gmail.com>
2024-08-19 11:52:11 -0600

    stuff

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,16 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  resolver: {
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'tsx', 'ts', 'jsx', 'js'], // Add extensions
+  },
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  entryFile: 'index.tsx', // Ensure index.tsx is recognized as the entry file
+};
+
+module.exports = mergeConfig(defaultConfig, config);
