48384c835de5874f35ef116a2e753cddf0524fa9
Brayden <braydenrhancock@gmail.com>
2024-08-19 12:14:01 -0600

    modules

--- metro.config.mjs
+++ metro.config.mjs
@@ -1,16 +1,10 @@
 import { getDefaultConfig, mergeConfig } from '@react-native/metro-config';
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
 const defaultConfig = getDefaultConfig(new URL('.', import.meta.url).pathname);
 
 const config = {
   resolver: {
-    sourceExts: [...defaultConfig.resolver.sourceExts, 'tsx', 'ts', 'jsx', 'js'], // Ensure TS and TSX files are recognized
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'tsx', 'ts', 'jsx', 'js'], // Add extensions
   },
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer'),
